0c9cffe493a4cd07fe86e6904c1451bbac1f7bf1
한동희 / 서비스개발담당 WEB개발팀 <214266@ssg.com>
2022-06-13 10:32:43 +0900

    feat: 테스트

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const memoize = require("@keyvhq/memoize");
 const Keyv = require("@keyvhq/core");
 const assert = require("assert");
 const getEtag = require("etag");
+const KeyvRedis = require("@keyv/redis");
 
 const { createKey, isFunction, setHeaders, size } = require("./util");
 
 const cacheableResponse = ({
   bypassQueryParameter = "force",
-  cache = new Keyv({ namespace: "ssr" }),
+  cache = new Keyv({
+    namespace: "ssr",
+    store: new KeyvRedis({ uri: "redis://:1111@127.0.0.1:6379" }),
+  }),
   compress: enableCompression = false,
   get,
   key: getKey = createKey(bypassQueryParameter),
